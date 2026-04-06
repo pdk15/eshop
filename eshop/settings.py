92e0ba329264ef52fdb66c771f91e0c2bc83a0b7
Pranav Khanolkar <pranavkhanolkar589@gmail.com>
2026-04-06 06:30:06 +0530

    fixed db url typo

--- eshop/settings.py
+++ eshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,13 +88,10 @@ RAZORPAY_KEY_SECRET = "YTiXRenzkppvzeQwmNKkZwMI"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 30,
-        },
-    }
+
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    )
     
 }
 
