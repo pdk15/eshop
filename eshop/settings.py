0266d767020cc95ee80436b884e95ae8c7d54d92
Pranav Khanolkar <pranavkhanolkar589@gmail.com>
2026-04-06 07:54:01 +0530

    fix migrations issue

--- eshop/settings.py
+++ eshop/settings.py
@@ -87,13 +87,19 @@ RAZORPAY_KEY_SECRET = "YTiXRenzkppvzeQwmNKkZwMI"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    )
-    
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get('DATABASE_URL')
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
